05b4e609fda220bf6410632d24934e580e6bfde1
Pitter Thog <blackthog@outlook.com>
2015-08-22 23:40:31 -0300

    Updated Javadoc and License declaration.

--- src/me/kaward/spongechat/events/ChannelMessageEvent.java
+++ src/me/kaward/spongechat/events/ChannelMessageEvent.java
@@ -1,22 +1,24 @@
 /**
- *  Spongechat — A new Powered Chat System for SpongePowered Minecraft API.
- *  Copyright (C) 2015 SparkPowered <https://github.com/SparkPowered/> and your contributors;
+ * 	Spongechat, a new powered chat system for SpongePowered Minecraft API.
+ * 	Copyright (C) 2015 Kaward <https://github.com/Kaward/>
+ * 	Copyright (C) 2015 SparkPowered <https://github.com/SparkPowered/>
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package me.kaward.spongechat.events;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -26,10 +28,23 @@ import org.spongepowered.api.event.AbstractEvent;
 import org.spongepowered.api.event.Cancellable;
 import org.spongepowered.api.text.Text;
 
-import me.kaward.spongechat.Message;
 import me.kaward.spongechat.Channel;
-import me.kaward.spongechat.Spongechat;
+import me.kaward.spongechat.Message;
+import me.kaward.spongechat.SpongechatAPI;
 
+/**
+ *
+ * <p>
+ * This is a abstract event factored with AbstractEvent from SpongePowered API.
+ * </p>
+ *
+ * <p>
+ * This event is called when player try to send a message, it can be handlered and cancelled if necessary by another plugins.
+ * </p>
+ *
+ * @category Event Factory
+ *
+ */
 public class ChannelMessageEvent extends AbstractEvent implements Cancellable
 {
 
@@ -49,47 +64,81 @@ public class ChannelMessageEvent extends AbstractEvent implements Cancellable
 		this.message = message;
 	}
 
-	public Set<UUID> getReceivers()
+	/**
+	 *
+	 * @return The list of players who receive the message.
+	 */
+	public Collection<UUID> getReceivers()
 	{
 		return receivers;
 	}
 
+	/**
+	 * <p>
+	 * Modify the text of the message.
+	 * </p>
+	 *
+	 * @param text The new text value.
+	 */
 	public void setText(Text text)
 	{
 		this.text = text;
 	}
 
+	/**
+	 *
+	 * @return Returns the Text instance of message.
+	 */
 	public Text getText()
 	{
 		return text;
 	}
 
+	/**
+	 *
+	 * @return The sender of messaghe.
+	 */
 	public Player getPlayer()
 	{
-		return (Player) Spongechat.sponge.getServiceManager().provide(Spongechat.class).get().getGame().getServer().getPlayer(getPlayerUniqueId());
+		return (Player) SpongechatAPI.getProvider().getGame().getServer().getPlayer(playerUniqueId);
 	}
 
+	/**
+	 *
+	 * @return The Message instance
+	 */
 	public Message getMessage()
 	{
 		return message;
 	}
 
-	public UUID getPlayerUniqueId()
-	{
-		return playerUniqueId;
-	}
-
+	/**
+	 *
+	 * @return The channel of message be sended.
+	 */
 	public Channel getChannel()
 	{
 		return channel;
 	}
 
+	/**
+	 *
+	 * @return If the event is cancelled (and message not be send) returns <b>true</b> and if not cancelled returns <b>false</b>
+	 *
+	 */
 	@Override
 	public boolean isCancelled()
 	{
 		return cancelled;
 	}
 
+	/**
+	 * <p>
+	 * The the event as cancelled (the message not be send).
+	 * </p>
+	 * 
+	 * @param cancelled <b>true</b> for cancel and <b>false</b> to set as not cancelled.
+	 */
 	@Override
 	public void setCancelled(boolean cancelled)
 	{
