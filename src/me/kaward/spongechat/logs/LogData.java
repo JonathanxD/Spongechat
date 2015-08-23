82a51d834dcbf3ce09dc4af5b22df9a7e00b7064
Pitter Thog <blackthog@outlook.com>
2015-08-22 23:24:31 -0300

    Updated the Javadoc and License declaration.

--- src/me/kaward/spongechat/logs/LogData.java
+++ src/me/kaward/spongechat/logs/LogData.java
@@ -1,5 +1,33 @@
+/**
+ * 	Spongechat, a new powered chat system for SpongePowered Minecraft API.
+ * 	Copyright (C) 2015 Kaward <https://github.com/Kaward/>
+ * 	Copyright (C) 2015 SparkPowered <https://github.com/SparkPowered/>
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package me.kaward.spongechat.logs;
 
+/**
+ *
+ * <p>
+ * This class is only used to store a log name and data of an item, as a instance.
+ * </p>
+ *
+ * @category Log Handlering
+ * @see me.kaward.spongechat.logs.LogItem The Item Class
+ *
+ */
 public class LogData
 {
 
@@ -17,11 +45,22 @@ public class LogData
 		this.data = data;
 	}
 
+	/**
+	 * <p>
+	 * Its a keyword, representer of data object.
+	 * </p>
+	 *
+	 * @return Keyword of data object.
+	 */
 	public String getName()
 	{
 		return name;
 	}
 
+	/**
+	 *
+	 * @return The data object.
+	 */
 	public Object getData()
 	{
 		return data;
